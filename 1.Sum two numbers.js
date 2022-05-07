276d1de2a9aaef6bf535e5795626f2d9866624f5
prakash535kumar <prakash535kumar@gmail.com>
2022-05-07 13:16:42 +0530

    First jschallenger program

---
+++ 1.Sum two numbers.js
@@ -0,0 +1,14 @@
+//Sum two numbers
+
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function myFunction
+    (a, b) {
+    return a + b;
+}
+
+myFunction(1,2)
+myFunction(1,10)
+myFunction(99,1)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Javascript-Basics
+This series of challenges covers the very basics of Javascript.
+
+I am practising JavaScript from https://www.jschallenger.com/javascript-basics
